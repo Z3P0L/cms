75e93f0df550d5ceb1f22cbd9aba7de2af021550
root <root@DESKTOP-DR6HUP3.localdomain>
2020-07-14 19:06:09 -0500

    First Commit

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+    <form method="POST" actio="login.php">
+        <h1>Login</h1>
+        <br>User <input type="text" name="user"></br>
+        <br>Password <input type="password" name="password"></br>
+        <br><input type="submit" value="Register"></br>
+<?php
+
+$user
+
+?>
+</html>
---
+++ register.php
@@ -0,0 +1,18 @@
+<html>
+<head><title>Formulario</title></head>
+<body>
+    <form method="POST" action="login.php">
+        <h1>Register</h1>
+        <br>User <input type="text" name="user"></br>
+        <br>Email <input type="email" name="email"></br>
+        <br>Password <input type="password" name="password"></br>
+        <br><input type="submit" value="Register"></br>
+    </form>
+
+</body>
+<?php
+
+
+
+?>
+</html>
