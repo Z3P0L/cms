5865dc3485fe9822428b3bf784acb8d875c0bfed
root <root@DESKTOP-DR6HUP3.localdomain>
2020-07-14 19:16:46 -0500

    Fix v1

--- login.php
+++ login.php
@@ -1,9 +1,19 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CMS</title>
+</head>
+<body>
+
     <form method="POST" actio="login.php">
         <h1>Login</h1>
         <br>User <input type="text" name="user"></br>
         <br>Password <input type="password" name="password"></br>
         <br><input type="submit" value="Register"></br>
+  
+</body>
 <?php
 
 $user
--- register.php
+++ register.php
@@ -1,6 +1,12 @@
-<html>
-<head><title>Formulario</title></head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CMS</title>
+</head>
 <body>
+
     <form method="POST" action="login.php">
         <h1>Register</h1>
         <br>User <input type="text" name="user"></br>
